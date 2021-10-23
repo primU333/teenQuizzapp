c9ea073155272d0a1b54e374701aa4e77dff91b7
Olawale O Samson <smsnmicheal@gmail.com>
2021-10-24 00:44:50 +0100

    ui changes

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
-//Display Navigation on mobile//
+//Hamburger menu
+const menuBtn      = document.querySelector('.menu-btn');
+const menu         = document.querySelector('.menu');
+const menuNav      = document.querySelector('.menu-nav');
+const navItems     = document.querySelectorAll('.nav-item');
 
-const nav = () =>{
-	const burger = document.querySelector(".burger");
-	const links = document.querySelector(".navlinks");
+let showMenu = false;
 
-	burger.addEventListener("click", ()=>{
-		links.classList.toggle("navlinks-open");
-	})
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu){
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        showMenu = false;
+    }
 }
 
-nav();
+//Dark mode switch
+const darkMode = document.querySelector('.dark-mode-switch');
+const switchBtn = document.querySelector('.switch');
+const body = document.querySelector('body');
+const footer = document.querySelector('footer');
 
-//Add dark mode class//
+let toggle = false;
 
-const change_mode = ()=>{
-	const dark_button = document.getElementById("dark-button");
-	const dark_mode = document.body;
+darkMode.addEventListener('click', toggleMode);
 
-	dark_button.addEventListener("click", (e)=>{
-		dark_mode.classList.toggle("dark");
-		//e.preventDefualt();
-	})
+function toggleMode() {
+	if(!toggle){
+		darkMode.style.transform = 'rotate(180deg)';
+		switchBtn.style.backgroundColor = '#000';
+		body.style.backgroundColor = '#777'
+		body.style.color = '#fff';
+		footer.style.backgroundColor = '#fff';
+		footer.style.color = '#000'
+
+		toggle = true;
+	} else {
+		darkMode.style.transform = 'rotate(0deg)';
+		switchBtn.style.backgroundColor ='#fff';
+		body.style.backgroundColor = '#f5fffa'
+		body.style.color = '#000';
+		footer.style.backgroundColor = '#444';
+		footer.style.color = '#fff'
+
+		toggle = false;
+	}
 }
-
-change_mode();
-
-
-//Show developer info/
-
-const show_info = ()=>{
-	const icon = document.getElementById("dev-icon");
-	const info = document.querySelector(".profile");
-
-	icon.addEventListener("click", ()=>{
-		info.classList.toggle("show-profile");
-	})
-}
-
-show_info();
-
-
-//show log in and sign up menu//
-
-const show_menu = ()=>{
-	const icon = document.getElementById("log-icon");
-	const info = document.querySelector(".logIn");
-
-	icon.addEventListener("click", ()=>{
-		info.classList.toggle("logIn-show");
-	})
-
-}
-
-show_menu();
-
-//Home Page relaod//
